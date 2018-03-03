df97b842881ff6974f382f97057a0a2f5ac4a1ef
Etienne Bazin <etienne.bazin@wifirst.fr>
2018-03-03 20:20:24 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+7.times do
+  FactoryBot.create(:character)
+end
--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -2,9 +2,9 @@ IMAGES_FIXTURE_ROOT = Rails.root.join('spec/fixtures/images')
 
 FactoryBot.define do
   factory :character do
-    name ['Xena', 'Dark Vador', 'The Warrior', 'Gargamel', 'Jean-Paul'].sample
-    toughness (1..10).to_a.sample
-    power (1..10).to_a.sample
+    name { ['Xena', 'Dark Vador', 'The Warrior', 'Gargamel', 'Jean-Paul'].sample }
+    toughness { (1..10).to_a.sample }
+    power { (1..10).to_a.sample }
     avatar { File.new(IMAGES_FIXTURE_ROOT.join(IMAGES_FIXTURE_ROOT.entries[2..-1].sample).to_s) }
   end
 end
