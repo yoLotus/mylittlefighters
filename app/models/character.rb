2b9809ede4dee7370f8b5aed6a6f37771d819c77
Etienne Bazin <etienne.bazin@wifirst.fr>
2018-03-04 11:45:14 +0100

    add validations to character models

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,11 @@
 class Character < ApplicationRecord
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  validates :name, :toughness, :power, presence: true
+  validates :toughness, :power, numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              less_than_or_equal_to: 10
+            }
 end
