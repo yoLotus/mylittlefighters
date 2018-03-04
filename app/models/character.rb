0c3158343c80f07693dd2bb0340bbf31354d5c51
Etienne Bazin <etienne.bazin@wifirst.fr>
2018-03-04 19:29:25 +0100

    show fights count into fighters overview

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,7 @@
 class Character < ApplicationRecord
+  has_many :victories, class_name: 'Fight', foreign_key: 'winner_id'
+  has_many :defeats, class_name: 'Fight', foreign_key: 'loser_id'
+
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
